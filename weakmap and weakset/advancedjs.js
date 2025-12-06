d2d62ca415e88bd0b6aead4209300a0f63edb5cd
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-12-06 22:15:37 +0530

    Refactor code to include WeakSet example

--- weakmap and weakset/advancedjs.js
+++ weakmap and weakset/advancedjs.js
@@ -1,3 +1,4 @@
+// weakmap
 let weak = new WeakMap();
 
 let obj = {key: "Roshan"};
@@ -5,4 +6,16 @@ let obj = {key: "Roshan"};
 weak.set(obj, "Employee");
 
 obj = null
+
 console.log(weak.get(obj))
+
+// weakset 
+
+let weakset = new WeakSet();
+
+let obj = {name: "Roshan"}
+
+weakset.add(obj);
+obj = null
+
+console.log(weakset.has(obj))
