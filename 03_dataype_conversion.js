97c62a2e639b57330cfe6b12fb28c9a3c3dad7b9
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-12-19 20:02:31 +0530

    datatype conversion

---
+++ 03_dataype_conversion.js
@@ -0,0 +1,33 @@
+// let score1 = "33"
+// let score2 = "33abc"
+
+// console.log(typeof score1)
+// console.log(typeof (score2))
+
+// let intDatatype1 = Number(score1);
+// console.log(typeof intDatatype1)
+// console.log(intDatatype1)
+
+// console.log("here is the difference ")
+
+
+// let intDatatype2 = Number(score2);
+// console.log(typeof intDatatype2) // typr of NaN is number 
+// console.log(intDatatype2)
+
+
+// let isloogedIn = 1; // true
+// let isloogedIn = 0 // false
+// let isloogedIn = "Roshan" // true
+let isloogedIn = "" // false
+ let booleanIsLoggedIn = Boolean(isloogedIn);
+
+//  console.log(booleanIsLoggedIn);
+
+
+let number = 33
+
+let stringNumber = String(number);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
