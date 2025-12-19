f597436892d50e2054692b422aeb58893fe58a94
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-12-19 20:01:34 +0530

    let const var

---
+++ 01_let_const_var.js
@@ -0,0 +1,19 @@
+const name = "Roshan"
+let roll = 421
+var id = "ma5055"
+let ram;
+
+// console.log(roll);
+// console.log(id)
+// console.log(name)
+// or we can use the below method to print everything at once
+
+console.table([name, roll, id,ram]); // here ram will be printed as undefined cause it is declared but not initialized so it will give undefined which is the default value provided to js 
+
+{} // this is called scope , whatever we will write in this code that will be in one scope and similarly for others 
+
+/*
+Prefer not to use the var because o block scope
+*/
+
+
