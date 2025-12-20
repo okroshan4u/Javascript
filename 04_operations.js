287f0bbe73b4d4de1b957337d4cb4cd66dc5bf2a
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-12-20 22:06:33 +0530

    operations in js

---
+++ 04_operations.js
@@ -0,0 +1,35 @@
+let value = 3
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(3+3)
+// console.log(3-3)
+// console.log(3*3)
+// console.log(3/3)
+// console.log(3%3)
+// console.log(3**2)
+
+let str1 = "Hello"
+let str2 = " Roshan"
+let str3 = str1 + str2 // gere we are concatinating the strings
+// console.log(str3)
+
+// console.log( typeof ("1"+ 2))
+// console.log(typeof ("Roshan"+ 2))
+// console.log("1"+ "2")
+
+// console.log("1"+2+2); //122 => As soon as a string enters the + chain, everything to the right becomes a string.
+// console.log(1+2+"2"); // 32
+
+// console.log(true)
+// console.log(+true)
+// console.log(true + 1)
+// console.log(+false)
+// console.log(+"")
+
+let incnum = 1
+incnum+
+console.log(incnum) // 1
+incnum++
+console.log(incnum) // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment     <= refer this link for more info about the increment operator
