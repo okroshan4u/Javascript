bf9f918c5a9fc805e96385c5225f25bcfb6d07e6
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-12-25 22:09:32 +0530

    IIFE in js

---
+++ 15_IIFE.js
@@ -0,0 +1,17 @@
+// (function log(){                       // this is IIFE 
+//     console.log("This is Roshan");
+// })()
+
+
+// ( ()=>{
+//     console.log("DB is connected successfully");  // ()() this is IIFE 
+// })()
+
+
+// ( (name)=>{
+//     console.log(`Hello  ${name}`);  // ()() this is IIFE 
+// })("Roshan") // here we can pass the arguments the function to the funtion
+
+
+
+
