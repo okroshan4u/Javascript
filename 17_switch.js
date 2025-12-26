9164cdda5f7f7cc90a443c496294dab5626de159
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-12-26 22:06:49 +0530

    switch statements in js

---
+++ 17_switch.js
@@ -0,0 +1,41 @@
+// const nw = 10;
+
+// switch (nw) {
+//     case 1:
+//         console.log("value is ",1)
+//         break;
+//     case 3:
+//         console.log("value is ",3)
+//         break;
+//     case 7:
+//         console.log("value is ",7)
+//         break;
+//     // case 90:
+//     //     console.log("value is ",90)
+//     //     break;
+//     case 10:
+//         console.log("value is ",10)
+//         break;
+
+//     default:
+//         console.log("Nothing found")
+//         break;
+// }
+
+const month = "April";
+
+switch (month.toLocaleLowerCase()) {
+    case "january":
+        console.log("The month is 1")
+        break;
+    case "march":
+        console.log("The month is 3")
+        break;
+    case "april":
+        console.log("The month is 4")
+        break;
+
+    default:
+        console.log("Nothing found")
+        break;
+}
