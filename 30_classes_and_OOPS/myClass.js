1ca453546e6cc505b515f1b9078eaa30c07f378a
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-01-02 22:22:25 +0530

    classes in js

---
+++ 30_classes_and_OOPS/myClass.js
@@ -0,0 +1,15 @@
+// ES6 ==> these all came after the es6
+class user{
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+}
+
+const userOne = new user("Roshan","okroshan4u@gmail.com", "12354")
+
+console.log(userOne.encryptPassword())
