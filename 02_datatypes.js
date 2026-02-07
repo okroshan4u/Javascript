aec22cbcd1124c0d9524dd6a902d7d4bbc9432d4
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-02-07 23:21:49 +0530

    Update 02_datatypes.js

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,2 +1,2 @@
-console.log(typeof null);  // object ==> It’s a bug from the very first version of JavaScript, and it was never fixed for backward compatibil
+console.log(typeof null);  // object ==> It’s a bug from the very first version of JavaScript, and it was never fixed for backward compatibi
 console.log(typeof undefined);
