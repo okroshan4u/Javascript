f9a8d71b6cb59a27edead49a8f0df84387346e9d
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-09-17 21:56:25 +0530

    Reformat 07_nums.js with consistent spacing

--- 07_nums.js
+++ 07_nums.js
@@ -4,7 +4,7 @@ const score = 2502
 const balance = new Number(100)
 // console.log(balance);
 
-// console.log(balance.toString().length)
+// console.log(balance.toString().length);
 // console.log(balance.toFixed(2))
 const otherNumber = 23.8966
 
