f09dc770b70352dd64466c9c234567b3138cf9c9
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-09-17 21:56:04 +0530

    Uncomment code in 07_nums.js for number operations

--- 07_nums.js
+++ 07_nums.js
@@ -2,7 +2,7 @@ const score = 2502
 // console.log(score);
 
 const balance = new Number(100)
-// console.log(balance)
+// console.log(balance);
 
 // console.log(balance.toString().length)
 // console.log(balance.toFixed(2))
