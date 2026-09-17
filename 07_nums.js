b48dfeb60b578d5ace8b85b083fc7c36c5f90eb9
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-09-17 21:57:03 +0530

    Uncomment code for number operations and random generation

--- 07_nums.js
+++ 07_nums.js
@@ -11,7 +11,7 @@ const otherNumber = 23.8966
 // console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'))
+// console.log(hundreds.toLocaleString('en-IN'));
 
 // ++++++++++++++Math++++++++++++++++++++++++
 // console.log(Math)
