115c2caaf96039040fd2f813ab55c6b1eeec7afc
Roshan <155551955+okroshan4u@users.noreply.github.com>
2026-09-20 21:51:08 +0530

    Fix console log formatting in 02_datatypes.js

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,2 +1,2 @@
-console.log(typeof null);  // object ==> It’s a bug from the very first version of JavaScript, and it was never fixed for backward
+console.log(typeof null);  // object ==> It’s a bug from the very first version of JavaScript, and it was never fixed for back
 console.log(typeof undefined);
